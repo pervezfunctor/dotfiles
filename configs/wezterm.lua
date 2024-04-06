1bb3c128163728a391fae51605bae00dc0ab7d81
Pervez Iqbal <pervezfunctor@gmail.com>
2024-04-06 22:11:40 +0530

    wezterm config

---
+++ configs/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Batman'
+
+config.font = wezterm.font 'MonaspiceRn Nerd Font Mono'
+
+config.font_size = 15.0
+
+return config
