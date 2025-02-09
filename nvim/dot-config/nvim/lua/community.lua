a7ef292f429e41da5c7ec8f0245734db5052ea67
Pervez Iqbal <pervefunctor@gmail.com>
2025-02-09 16:32:42 +0530

    disable most astro community plugins by default

--- nvim/dot-config/nvim/lua/community.lua
+++ nvim/dot-config/nvim/lua/community.lua
@@ -6,23 +6,20 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.cpp" },
   -- { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.pack.tailwindcss" },
+  -- { import = "astrocommunity.pack.json" },
+  -- { import = "astrocommunity.pack.python-ruff" },
+  -- { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.utility.hover-nvim" },
   { import = "astrocommunity.recipes.vscode" },
-
+  --
   -- import/override with your plugins folder
 }
