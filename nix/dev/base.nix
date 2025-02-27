7488d327a293bc4ca2003a8f0fabafcadc239029
Pervez Iqbal <pervezfunctor@gmail.com>
2025-02-27 22:58:06 +0530

    nix setup

--- nix/dev/base.nix
+++ nix/dev/base.nix
@@ -3,21 +3,6 @@
 pkgs.mkShell {
   name = "base-shell";
   buildInputs = [
-    pkgs.trash-cli
-    pkgs.gh
-    pkgs.just
-    pkgs.lazygit
-    pkgs.eza
-    pkgs.tmux
-    pkgs.neovim
-    pkgs.starship
-    pkgs.zsh
-    pkgs.delta
-    pkgs.fzf
-    pkgs.fd
-    pkgs.zoxide
-    pkgs.bat
-    pkgs.ripgrep
     # pkgs.git
     # pkgs.curl
     # pkgs.wget
