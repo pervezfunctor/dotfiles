fdfce975d414a11644c488b4e4425305b5a11d3f
Pervez Iqbal <pervezfunctor@gmail.com>
2025-03-01 03:39:05 +0530

    minor fix

--- home-manager/dot-config/home-manager/home.nix
+++ home-manager/dot-config/home-manager/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "pervez";
-  home.homeDirectory = "/home/pervez";
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
