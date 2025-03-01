45950109ad04e2fe00255ab583f2d0b93616f94d
Pervez Iqbal <pervezfunctor@gmail.com>
2025-03-01 17:53:45 +0530

    nix group install instead of nix-setup

--- home-manager/dot-config/home-manager/home.nix
+++ home-manager/dot-config/home-manager/home.nix
@@ -1,22 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = builtins.getEnv "USER";
   home.homeDirectory = builtins.getEnv "HOME";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.zsh
     pkgs.trash-cli
@@ -36,10 +28,6 @@
     pkgs.tmux
     pkgs.neovim
 
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -54,24 +42,22 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
-    # ".zshrc" = { source = ~/.ilm/zsh/dot-zshrc; };
-    # ".config/nvim" = { source = ~/.ilm/nvim/dot-config/nvim; };
-    # ".config/tmux" = {
-    #     source = ~/.ilm/tmux/dot-config/tmux;
-    #     recursive = true;
-    # };
-    # ".gitconfig" = {
-    #     source = ~/.ilm/git/dot-gitconfig;
-    #     copy = true;
-    # };
+    ".zshrc" = { source = ~/.ilm/zsh/dot-zshrc; };
+    ".config/nvim" = { source = ~/.ilm/nvim/dot-config/nvim; };
+    ".config/tmux" = {
+        source = ~/.ilm/tmux/dot-config/tmux;
+        recursive = true;
+    };
+    ".gitconfig" = {
+        source = ~/.ilm/git/dot-gitconfig;
+        copy = true;
+    };
     # ".config/Code/User/settings.json" = {
     #     source = ~/.ilm/extras/vscode/minimal-settings.json;
     #     copy = true;
@@ -111,11 +97,5 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # programs.zsh.enable = true;
-  # home.shellPath = "${pkgs.zsh}/bin/zsh";
-
-  programs.bash.initExtra = ''
-      [ -f ~/.ilm/share/bashrc ] && source ~/.ilm/share/bashrc
-      has_cmd starship && eval "$(starship init bash)"
-    '';
+  programs.zsh.enable = true;
 }
