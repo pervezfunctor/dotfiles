b99f2b7814dc653fc2094c6cab7fde0eaedd46b5
Pervez Iqbal <pervefunctor@gmail.com>
2025-03-06 14:48:14 +0530

    let wezterm have title bar

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
-	font_size = 12.0,
+	font_size = 10.0,
 	font = wezterm.font('JetBrains Mono'),
 	macos_window_background_blur = 30,
 	window_background_opacity = 0.9,
-	window_decorations = 'RESIZE',
+	-- window_decorations = 'RESIZE',
 	window_close_confirmation = 'NeverPrompt',
 	-- keys = {
 		-- {
