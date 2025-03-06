ebb0f3a4b86fde15b9c8bbb8a77ac6b2c6d962ce
Pervez Iqbal <pervefunctor@gmail.com>
2025-03-06 17:50:09 +0530

    minor changes

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,15 +1,25 @@
 local wezterm = require 'wezterm'
 
 return {
+	color_scheme = 'Everforest Dark (Gogh)',
+	-- color_scheme = 'Catpaccuing Mocha',
+	-- color_scheme = 'Rosé Pine Dawn (Gogh)',
+	-- color_scheme = 'Rosé Pine Dawn (Gogh)',
+	-- color_scheme = 'Everforest Light (Gogh)',
+	-- config.color_scheme = 'Github Dark (Gogh)',
+
 	adjust_window_size_when_changing_font_size = false,
-	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 10.0,
 	font = wezterm.font('JetBrains Mono'),
 	macos_window_background_blur = 30,
 	window_background_opacity = 0.9,
-	-- window_decorations = 'RESIZE',
 	window_close_confirmation = 'NeverPrompt',
+
+	-- window_decorations = 'RESIZE',
+	-- window_background_image = '/path/to/wallpaper.jpg'
+	-- text_background_opacity = 0.3
+
 	-- keys = {
 		-- {
 		-- 	key = 'q',
