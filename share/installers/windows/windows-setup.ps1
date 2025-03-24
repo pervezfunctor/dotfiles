9df9042c676266b1925ae872776dce7738624411
Pervez Iqbal <pervezfunctor@gmail.com>
2025-03-24 23:46:32 +0530

    minor fixes

--- share/installers/windows/windows-setup.ps1
+++ share/installers/windows/windows-setup.ps1
@@ -130,6 +130,8 @@ function Install-NerdFonts {
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     }
 
     # Install Nerd Fonts
