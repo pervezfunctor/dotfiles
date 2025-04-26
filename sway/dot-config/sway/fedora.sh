1c6278cba394ec5d138c9bf3ab42be8b5ab27bb0
Pervez Iqbal <pervefunctor@gmail.com>
2025-04-26 11:06:57 +0530

    minor sway config fixes

---
+++ sway/dot-config/sway/fedora.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# shellcheck disable=SC1091
+source "$HOME"/.ilm/share/utils
+
+is_fedora || return
+
+/usr/bin/xdg-user-dirs-update
+/usr/libexec/sway-systemd/wait-sni-ready && systemctl --user start sway-xdg-autostart.target
+/usr/libexec/sway-systemd/assign-cgroups.py
+/usr/libexec/sway-systemd/session.sh
