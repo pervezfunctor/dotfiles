989025934876e3e4d5424e2832c40ff84f5212e2
Pervez Iqbal <pervefunctor@gmail.com>
2025-07-04 21:00:22 +0530

    nixos-vm config

---
+++ nix/nixos-confugration.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  system.stateVersion = "24.05";
+
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "nixos";
+  };
+
+  environment.systemPackages = with pkgs; [
+    bat
+    carapace
+    curl
+    delta
+    emacs-nox
+    eza
+    fd
+    fzf
+    gcc
+    gh
+    git
+    gnumake
+    htop
+    just
+    lazygit
+    luarocks
+    neovim
+    nixfmt-classic
+    nushell
+    ripgrep
+    sd
+    starship
+    stow
+    tmux
+    trash-cli
+    tree
+    unzip
+    wget
+    wget
+    zoxide
+    zsh
+
+  ];
+
+  imports = [ <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix> ];
+
+  virtualisation.qemu.options = [
+    "-device virtio-vga"
+
+  ];
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+}
