2391ebdf9f49ca3505281915b839b5f8f9232608
Pervez Iqbal <pervefunctor@gmail.com>
2025-09-28 04:42:41 +0530

    minor changes

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs }:
+    {
+      nixpkgs,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -14,28 +18,19 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          curl
-          docker-client
           eza
           fzf
           gh
-          git
-          go
-          jq
           micro
-          neovim
-          nixd
-          nixfmt-rfc-style
-          nodejs
-          python3
-          ripgrep
           shellcheck
           shfmt
           stow
-          tmux
           zoxide
-          zsh
         ];
+      };
+
+      shellHooks = {
+        default = "echo 'Welcome to the devcontainer!'";
       };
     };
 }
