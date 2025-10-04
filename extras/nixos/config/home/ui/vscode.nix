fc19e0445ef614d5243e3a9fd585d1fa82bc174a
Pervez Iqbal <pervezfunctor@gmail.com>
2025-10-05 04:26:25 +0530

    nixos: disable vscode config management

--- extras/nixos/config/home/ui/vscode.nix
+++ extras/nixos/config/home/ui/vscode.nix
@@ -31,49 +31,49 @@
         # sclu1034.justfile
       ];
 
-      userSettings = {
-        "editor.acceptSuggestionOnEnter" = "smart";
-        "editor.autoIndent" = "advanced";
-        "editor.cursorSmoothCaretAnimation" = "on";
-        "editor.find.autoFindInSelection" = "never";
-        "editor.fontFamily" = "'JetbrainsMono Nerd Font Mono', 'monospace'";
-        "editor.fontSize" = 15;
-        "editor.formatOnPaste" = true;
-        "editor.formatOnSave" = true;
-        "editor.indentSize" = "tabSize";
-        "editor.detectIndentation" = false;
-        "editor.insertSpaces" = true;
-        "editor.minimap.enabled" = false;
-        "editor.renderWhitespace" = "boundary";
-        "editor.rulers" = [ 80 ];
-        "editor.smoothScrolling" = true;
-        "editor.snippetSuggestions" = "bottom";
-        "editor.tabCompletion" = "on";
-        "editor.tabSize" = 2;
-        "editor.trimAutoWhitespace" = true;
+      #   userSettings = {
+      #     "editor.acceptSuggestionOnEnter" = "smart";
+      #     "editor.autoIndent" = "advanced";
+      #     "editor.cursorSmoothCaretAnimation" = "on";
+      #     "editor.find.autoFindInSelection" = "never";
+      #     "editor.fontFamily" = "'JetbrainsMono Nerd Font Mono', 'monospace'";
+      #     "editor.fontSize" = 15;
+      #     "editor.formatOnPaste" = true;
+      #     "editor.formatOnSave" = true;
+      #     "editor.indentSize" = "tabSize";
+      #     "editor.detectIndentation" = false;
+      #     "editor.insertSpaces" = true;
+      #     "editor.minimap.enabled" = false;
+      #     "editor.renderWhitespace" = "boundary";
+      #     "editor.rulers" = [ 80 ];
+      #     "editor.smoothScrolling" = true;
+      #     "editor.snippetSuggestions" = "bottom";
+      #     "editor.tabCompletion" = "on";
+      #     "editor.tabSize" = 2;
+      #     "editor.trimAutoWhitespace" = true;
 
-        "files.autoSave" = "onFocusChange";
-        "files.insertFinalNewline" = true;
-        "files.trimTrailingWhitespace" = true;
+      #     "files.autoSave" = "onFocusChange";
+      #     "files.insertFinalNewline" = true;
+      #     "files.trimTrailingWhitespace" = true;
 
-        "git.autofetch" = true;
+      #     "git.autofetch" = true;
 
-        "nix.enableLanguageServer" = true;
-        "nix.formatterPath" = "alejandra";
-        "nix.serverPath" = "nixd";
+      #     "nix.enableLanguageServer" = true;
+      #     "nix.formatterPath" = "alejandra";
+      #     "nix.serverPath" = "nixd";
 
-        "update.mode" = "manual";
+      #     "update.mode" = "manual";
 
-        "window.titleBarStyle" = "custom";
+      #     "window.titleBarStyle" = "custom";
 
-        "workbench.colorTheme" = "Catppuccin Mocha";
-        "workbench.sideBar.location" = "right";
-        "docker.extension.enableComposeLanguageServer" = true;
-        "github.copilot.enable" = {
-          "*" = false;
-        };
-        "workbench.startupEditor" = "none";
-      };
+      #     "workbench.colorTheme" = "Catppuccin Mocha";
+      #     "workbench.sideBar.location" = "right";
+      #     "docker.extension.enableComposeLanguageServer" = true;
+      #     "github.copilot.enable" = {
+      #       "*" = false;
+      #     };
+      #     "workbench.startupEditor" = "none";
+      #   };
     };
   };
 }
