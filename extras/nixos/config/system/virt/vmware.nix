365f0e9cf4d0ad2d9ef3dde592fa04cedff23b2e
Pervez Iqbal <pervefunctor@gmail.com>
2025-10-12 00:15:07 +0530

    minor change

--- extras/nixos/config/system/virt/vmware.nix
+++ extras/nixos/config/system/virt/vmware.nix
@@ -2,8 +2,4 @@
 
 {
   virtualisation.vmware.host.enable = true;
-  virtualisation.vmware.host.extraConfig = "
-  # Allow unsupported device's OpenGL and Vulkan acceleration for guest vGPU
-  mks.gl.allowUnsupportedDrivers = \"TRUE\"\n
-  mks.vk.allowUnsupportedDevices = \"TRUE\"\n";
 }
