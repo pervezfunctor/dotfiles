0fd161af94017d6b1f65836647fa5435c48c15c7
Pervez Iqbal <pervezfunctor@gmail.com>
2025-12-18 20:09:08 +0530

    WIP niri/hyprland dms/noctalia config

--- extras/nixos/config/system/niri.nix
+++ extras/nixos/config/system/niri.nix
@@ -1,5 +1,37 @@
 { ... }:
 {
   # services.displayManager.gdm.enable = true;
-  programs.niri.enable = true;
+  programs.niri = {
+    enable = true;
+    settings = {
+      input = {
+        keyboard = {
+          xkb.layout = "us";
+          xkb.kb_options = "caps:ctrl_modifier";
+        };
+      };
+
+      layout = {
+        gaps = 8;
+      };
+
+      bar = {
+        enable = false;
+
+      };
+
+      binds = {
+        "Mod+Return" = {
+          spawn = "ghostty";
+        };
+        "Mod+Q" = {
+          close-window = true;
+        };
+
+        "Mod+Shift+E" = {
+          quit = true;
+        };
+      };
+    };
+  };
 }
